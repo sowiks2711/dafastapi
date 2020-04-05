d00a7ea16332daa95265e45ad2091d3a0f98e469
sowiks <sowiks@student.mini.pw.edu.pl>
2020-04-06 01:31:39 +0200

    Add solution

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.exceptions import HTTPException
 app = FastAPI()
+
+
+class PatientIdCounter():
+    def __init__(self, idCounter):
+        self.idCounter = idCounter
+        self.patients = dict()
+
+
+patientIdCounter = PatientIdCounter(idCounter=0)
+
+
+class HelloNameResp(BaseModel):
+    message: str
+
+
+class MethodResp(BaseModel):
+    method: str
+
+
+class PatientRequest(BaseModel):
+    name: str
+    surname: str
+
+
+class PatientResponse(BaseModel):
+    id: int
+    patient: PatientRequest
 
 
 @app.get('/')
@@ -8,11 +36,41 @@ def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-class HelloNameResp(BaseModel):
-    message: str
-
-
 @app.get('/hello/{name}', response_model=HelloNameResp)
 def hello_name(name: str):
     return HelloNameResp(message=f"Hello {name}!")
 
+
+@app.get('/method', response_model=MethodResp)
+def method_get():
+    return MethodResp(method="GET")
+
+
+@app.put('/method', response_model=MethodResp)
+def method_put():
+    return MethodResp(method="PUT")
+
+
+@app.post('/method', response_model=MethodResp)
+def method_post():
+    return MethodResp(method="POST")
+
+
+@app.delete('/method', response_model=MethodResp)
+def method_delete():
+    return MethodResp(method="DELETE")
+
+
+@app.post('/patient', response_model=PatientResponse)
+def patient(patient: PatientRequest):
+    id = patientIdCounter.idCounter
+    patientIdCounter.idCounter += 1
+    patientIdCounter.patients.update({id: patient})
+    return PatientResponse(id=id, patient=patient)
+
+
+@app.get('/patient/{id}')
+def get_patient(id: int):
+    if id not in patientIdCounter.patients:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return patientIdCounter.patients[id]
--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 import pytest
+import json
 
 client = TestClient(app)
 
@@ -11,6 +12,30 @@ def test_hello_world():
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
 
 
+def test_method_get():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+
+def test_method_post():
+    response = client.post('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "POST"}
+
+
+def test_method_put():
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+
+def test_method_delete():
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+
+
 @pytest.mark.parametrize(
     "name",
     ["Ala", "Zażółć gęślą jaźń", "Grzegorz Brzęczyszczykiewicz"]
@@ -19,3 +44,39 @@ def test_hello_name(name):
     response = client.get(f'/hello/{name}')
     assert response.status_code == 200
     assert response.json() == {"message": f"Hello {name}!"}
+
+
+@pytest.mark.parametrize(
+    "name,surname,expected",
+    [("Ala", "lala", 0), ("Edward", "Zażółć gęślą jaźń", 1),
+     ("Grzegorz", "Brzęczyszczykiewicz", 2)]
+)
+def test_patient(name, surname, expected):
+    response = client.post(
+        f'/patient',
+        json.dumps({"name": name, "surname": surname})
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": expected, "patient": {"name": name, "surname": surname}
+    }
+
+
+@pytest.mark.parametrize(
+    "name,surname,id",
+    [("Ala", "lala", 0), ("Edward", "Zażółć gęślą jaźń", 1),
+     ("Grzegorz", "Brzęczyszczykiewicz", 2)]
+)
+def test_get_patient_exist(name, surname, id):
+    response = client.get(f'/patient/{id}')
+    assert response.status_code == 200
+    assert response.json() == {"name": name, "surname": surname}
+
+
+@pytest.mark.parametrize(
+    "id",
+    [100, 101, -2]
+)
+def test_get_patient_not_exists(id):
+    response = client.get(f'/patient/{id}')
+    assert response.status_code == 404
